ba162e74f377f20b883a757c1b8b4b3f2cbe0756
Jim LeBeau <jrlebeau@yahoo.com>
2019-04-03 13:18:41 -0700

    with application properties

--- src/main/java/com/jrl/springboot/application/DemoApplication.java
+++ src/main/java/com/jrl/springboot/application/DemoApplication.java
@@ -1,23 +1,38 @@
 package com.jrl.springboot.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	
+	@Autowired
+	RestTemplate restTemplate;
 
+	@Value("${spring.application.name}")
+	private String name;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
 	}
 	
 	@RequestMapping(value = "/")
-	public String hello() {
-		return "Hello World";
+	public String name() {
+		return name;
+	}
+	@Bean
+	
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 	
-
+	
 }
