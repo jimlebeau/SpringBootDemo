de6c7e165cd16a940e52287e32154d520cc7d190
Jim LeBeau <jrlebeau@yahoo.com>
2019-04-04 08:11:00 -0700

    restful

--- src/main/java/com/jrl/springboot/application/DemoApplication.java
+++ src/main/java/com/jrl/springboot/application/DemoApplication.java
@@ -1,45 +1,54 @@
 package com.jrl.springboot.application;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
+import com.jrl.springboot.controller.ProductController;
+
 @SpringBootApplication
-@RestController
+@ComponentScan(basePackageClasses = ProductController.class)
 public class DemoApplication {
 	
 	@Autowired
 	RestTemplate restTemplate;
+	
 	@Value("${spring.application.name}")
 	private String appName;
 	
 	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+	private static ApplicationContext ctx;
 
 	public static void main(String[] args) {
 		logger.info("this is a info message");
 		logger.warn("this is a warn message");
 		logger.error("this is an error message");
-		SpringApplication.run(DemoApplication.class, args);
+		ctx = SpringApplication.run(DemoApplication.class, args);
+//		displayAllBeans();
 		
-
 	}
 	
-	@RequestMapping(value = "/")
-	public String hello() {
-		return appName;
+	public static void displayAllBeans() {
+		String[] allBeanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(allBeanNames);
+		for(String beanName : allBeanNames) {
+			System.out.println(beanName);
+		}
 	}
+	
 	@Bean
-	
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
-	
-	
+		
 }
